b1a3fe43d0e8bae2452dc4810ad79cf4346fb877
Alex King <calicoalix@gmail.com>
2022-06-29 21:46:06 +1000

    added streaming and rent availability sections on title page

--- src/pages/Title.tsx
+++ src/pages/Title.tsx
@@ -9,6 +9,8 @@ function Title() {
     const [filmId, setFilmId] = useState(Number)
     const [loading, setLoading] = useState(true)
     const [backdropLoaded, setBackdropLoaded] = useState(false)
+    const [services, setServices] = useState(Object)
+    const [fetchedServices, setFetchedServices] = useState(false)
     const location = useLocation()
 
 
@@ -16,7 +18,7 @@ function Title() {
         getTitleData()
     }, [filmId])
 
-    const getTitleData = () => {
+    const getTitleData = async () => {
         if (location.state !== null){
             const { movieInfo } = location.state
             // console.log("MOVIE INFO", movieInfo.backdrop_path)
@@ -29,7 +31,7 @@ function Title() {
             let movieId = parseInt(arr.join(''))
             setFilmId(movieId)
             console.log('location', movieId)
-            axios.get(URLS.HEAD + movieId + URLS.KEY)
+            await axios.get(URLS.HEAD + movieId + URLS.KEY)
             .then((res) => {
                 console.log(res.data)
                 setFilmData(res.data)
@@ -38,10 +40,48 @@ function Title() {
                 console.error(err)
             })
         }
+        await getServices(filmId)
         setLoading(false)
     }
-    // let filmData = 
-    // {   "adult": false,   "backdrop_path": "/sd4xN5xi8tKRPrJOWwNiZEile7f.jpg",   "belongs_to_collection": {     "id": 87118,     "name": "Cars Collection",     "poster_path": "/ooYvY9DMEdUHH2dOPHbZmyfMENy.jpg",     "backdrop_path": "/A8DqaTGwZ8iCEjWMNRsZumzfKLw.jpg"   },   "budget": 120000000,   "genres": [     {       "id": 16,       "name": "Animation"     },     {       "id": 12,       "name": "Adventure"     },     {       "id": 35,       "name": "Comedy"     },     {       "id": 10751,       "name": "Family"     }   ],   "homepage": "http://disney.go.com/disneyvideos/animatedfilms/cars/",   "id": 920,   "imdb_id": "tt0317219",   "original_language": "en",   "original_title": "Cars",   "overview": "Lightning McQueen, a hotshot rookie race car driven to succeed, discovers that life is about the journey, not the finish line, when he finds himself unexpectedly detoured in the sleepy Route 66 town of Radiator Springs. On route across the country to the big Piston Cup Championship in California to compete against two seasoned pros, McQueen gets to know the town's offbeat characters.",   "popularity": 83.826,   "poster_path": "/qa6HCwP4Z15l3hpsASz3auugEW6.jpg",   "production_companies": [     {       "id": 2,       "logo_path": "/wdrCwmRnLFJhEoH8GSfymY85KHT.png",       "name": "Walt Disney Pictures",       "origin_country": "US"     },     {       "id": 3,       "logo_path": "/1TjvGVDMYsj6JBxOAkUHpPEwLf7.png",       "name": "Pixar",       "origin_country": "US"     }   ],   "production_countries": [     {       "iso_3166_1": "US",       "name": "United States of America"     }   ],   "release_date": "2006-06-08",   "revenue": 461983149,   "runtime": 117,   "spoken_languages": [     {       "english_name": "English",       "iso_639_1": "en",       "name": "English"     },     {       "english_name": "Italian",       "iso_639_1": "it",       "name": "Italiano"     },     {       "english_name": "Japanese",       "iso_639_1": "ja",       "name": "日本語"     },     {       "english_name": "Yiddish",       "iso_639_1": "yi",       "name": ""     }   ],   "status": "Released",   "tagline": "Ahhh... it's got that new movie smell.",   "title": "Cars",   "video": false,   "vote_average": 6.9,   "vote_count": 11709 }
+   
+    const getServices = async (input:number) => {
+        if (input !== 0){
+
+            console.log(input)
+            await axios.get(URLS.HEAD + input + URLS.SERVICES)
+            .then((res) => {
+                console.log("RESULTS!!!", res.data.results.AU)
+                setServices(res.data.results.AU)
+            })
+            .catch((err) => {
+                console.error(err)
+            })
+            console.log('SERVICES:', services)
+            setFetchedServices(true)
+        }
+    }
+
+    const renderStreamingPlatforms = () => {
+        console.log(services.flatrate)
+        // let streaming = services.flatrate
+        return services.flatrate.map((service: any, idx: number) => {
+            return <div className="service-element" key={service.id} id={`${idx+1}`}>
+                <abbr title={service.provide_name}>
+                <img className="service-platform" src={URLS.POSTER + service.logo_path} />
+                </abbr>
+            </div>
+        })
+    }
+
+    const renderPurchasePlatforms = () => {
+        return services.rent.map((service: any, idx: number) => {
+            return <div className="service-element" key={service.id} id={`${idx+1}`}>
+                <abbr title={service.provide_name}>
+                <img className="service-platform" src={URLS.POSTER + service.logo_path} />
+                </abbr>
+            </div>
+        })
+    }
 
     return (
         <div className="film-page">
@@ -59,20 +99,56 @@ function Title() {
                         <div>Loading resources...</div>
                     </div>
                     
-                    <div className="search-element">
+                    <div className="">
                     </div>
-                    <div className="search-element" key={filmData.id} style={{display: backdropLoaded ? 'inline-block' : 'none'}}>
+                    <div className="title-block" key={filmData.id} style={{display: backdropLoaded ? 'grid' : 'none'}}>
                         <img className="title-img" src={URLS.POSTER + filmData.poster_path}/>
                         <img className="backdrop" src={URLS.BACKDROP + filmData.backdrop_path} onLoad={() => setBackdropLoaded(true)}/>
-                        <div className="user-rate">
-                            {filmData.vote_average} rate
-                        </div>
+
                         <div className="filmData-info">
                             <p className="title-name">{filmData.original_title}</p>
-                                
-                            <p className="overview">{filmData.overview}</p>
-                            <button>+ Watch list</button>
+                            <p className="synopsis">Synopsis:</p>
+                            <p className="title-overview">{filmData.overview}</p>
+
+                            <div className="where-to-watch">
+                                <p className="where-title">
+                                    Available to watch:
+                                </p>
+                                <div className="where-stream">
+                                    {
+                                        fetchedServices
+                                        ?
+                                        <div className="platforms">
+                                            <div className="streaming-platforms">
+                                                Stream:
+                                                {
+                                                    services.flatrate
+                                                    ?
+                                                    renderStreamingPlatforms()
+                                                    :
+                                                    <p>No streaming data found</p>
+                                                    
+                                                }
+                                            </div>
+                                            <div className="purchase-platforms">
+                                                Rent:
+                                                {
+                                                    services.rent
+                                                    ?
+                                                    renderPurchasePlatforms()
+                                                    :
+                                                    <p>No data found</p>
+                                                    
+                                                }
+                                            </div>
+                                        </div>
+                                        :
+                                        <p className=""></p>
+                                    }
+                                </div>
+                            </div>
                         </div>
+
                     </div>
                 </div>
                 // renderTitle
