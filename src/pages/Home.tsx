53a7a961d72641ed34c6a288f39650afd01b34bd
Alex King <calicoalix@gmail.com>
2022-06-29 23:35:13 +1000

    flipped logo gradient to be horizontal

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -61,7 +61,8 @@ function Home() {
                 <img className="res-img" src={URLS.POSTER + movie.poster_path}/>
                 <div className="movie-info">
                     <div className="user-rate">
-                        {movie.vote_average} rate
+                    <svg width="16" height="16" xmlns="http://www.w3.org/2000/svg" class="ipc-icon ipc-icon--star-inline" id="iconContext-star-inline" viewBox="0 0 24 24" fill="currentColor" role="presentation"><path d="M12 20.1l5.82 3.682c1.066.675 2.37-.322 2.09-1.584l-1.543-6.926 5.146-4.667c.94-.85.435-2.465-.799-2.567l-6.773-.602L13.29.89a1.38 1.38 0 0 0-2.581 0l-2.65 6.53-6.774.602C.052 8.126-.453 9.74.486 10.59l5.147 4.666-1.542 6.926c-.28 1.262 1.023 2.26 2.09 1.585L12 20.099z"></path></svg>
+                        {movie.vote_average} 
                     </div>
                     <Link to={`/title?id=${movie.id}`} state={{movieInfo: movie}}>
                     <p className="film-title">{movie.original_title}</p>
@@ -75,6 +76,7 @@ function Home() {
 
     const scrollGroup = (elem:string, right:boolean) => {
         let target = document.getElementById(elem)
+        console.log(target ,document.documentElement.clientWidth)
         if (target){
             let scrollVal = (document.documentElement.clientWidth)
             if (scrollCount === 4 && right){
