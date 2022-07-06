c6e7f16d3a457b9a42dbfa8dc3b221c962427b6b
Alex King <calicoalix@gmail.com>
2022-07-06 14:46:57 +1000

    search option button styled correctly - fixed flex box issues with the magnify svg

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -4,11 +4,12 @@ import axios from 'axios'
 import URLS from '../api/movieApi'
 import React from 'react'
 import '../style/Search.css'
+import movieImg from '../images/film.png'
 
 
 function Search() {
     const [query, setQuery] = useState("")
-    const [searchedQuery, setSearchedQuery] = useState("")
+    const [searchSelected, setSearchSelected] = useState(true)
 
     const newQuery = (event: React.ChangeEvent<HTMLInputElement>) => {
         event.preventDefault()
@@ -17,9 +18,15 @@ function Search() {
 
     return (
       <div className="Search">
-        <form >
+        <div className="navSearch">
 
-            <input name="search" className="search" placeholder={"Search for a title"} type={"text"} onChange={newQuery}></input>
+        <button className="search-option title-opt">
+            <img src={movieImg} className="search-opt-img"/>
+        </button>
+        <button className="search-option user-opt" style={{display: searchSelected ? 'none' : 'contents'}}></button>
+        
+        <form className="searchform">
+            <input name="search" className="search-input" placeholder={"Search for a title"} type={"text"} onChange={newQuery}></input>
             <Link to={`/title/search?query=${encodeURIComponent(query)}`} state={{searchQuery: decodeURIComponent(query)}}>
                 <button type="submit" className="search-butt">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" className="magnify" id="iconContext-magnify" viewBox="0 0 24 24" fill="black" role="presentation"><path fill="none" d="M0 0h24v24H0V0z"></path><path d="M15.5 14h-.79l-.28-.27a6.5 6.5 0 0 0 1.48-5.34c-.47-2.78-2.79-5-5.59-5.34a6.505 6.505 0 0 0-7.27 7.27c.34 2.8 2.56 5.12 5.34 5.59a6.5 6.5 0 0 0 5.34-1.48l.27.28v.79l4.25 4.25c.41.41 1.08.41 1.49 0 .41-.41.41-1.08 0-1.49L15.5 14zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path></svg>
@@ -27,6 +34,7 @@ function Search() {
 
             </Link>
         </form>
+        </div>
       </div>
     )
     
