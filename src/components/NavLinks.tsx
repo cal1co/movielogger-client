9c1ddaf8f109275fe2cfa743bd1710d5db09bf1f
Alex King <calicoalix@gmail.com>
2022-07-06 23:30:54 +1000

    logout successful without errors - need to route refresh page though to avoid errors on other pages

--- src/components/NavLinks.tsx
+++ src/components/NavLinks.tsx
@@ -11,13 +11,23 @@ function NavLinks() {
         console.log(JSON.parse(localStorage.getItem('currentUser') || '{}'))
     }, [name])
 
-
+    const logout = () => {
+        console.log("LOGGING OUT", name)
+        localStorage.removeItem('currentUserToken')
+        localStorage.removeItem('currentUserId')
+        localStorage.removeItem('currentUserName')
+        localStorage.removeItem('currentUser')
+    }
     return (
         <div className="nav-links">
             
             <Link to={`/${name}`} className="prof-link">
                 @{name}
             </Link>
+
+            <button className="logout-butt" onClick={logout}>
+                logout
+            </button>
 
         </div>
     )
