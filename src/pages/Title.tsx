6335527336fb5251cf7fb45c9e9e48db83caf5ef
Alex King <calicoalix@gmail.com>
2022-07-07 19:37:46 +1000

    changed title backdrop image to fit screen and never be smaller than the width of the viewport

--- src/pages/Title.tsx
+++ src/pages/Title.tsx
@@ -129,16 +129,8 @@ function Title() {
                         <img className="title-img" src={URLS.POSTER + filmData.poster_path}/>
                         <img className="backdrop" src={URLS.BACKDROP + filmData.backdrop_path} onLoad={() => setBackdropLoaded(true)}/>
                         
-{/*                         
-                        <path d="M12 17.27l4.15 2.51c.76.46 1.69-.22 1.49-1.08l-1.1-4.72 3.67-3.18c.67-.58.31-1.68-.57-1.75l-4.83-.41-1.89-4.46c-.34-.81-1.5-.81-1.84 0L9.19 8.63l-4.83.41c-.88.07-1.24 1.17-.57 1.75l3.67 3.18-1.1 4.72c-.2.86.73 1.54 1.49 1.08l4.15-2.5z"></path>
-                         */}
-                        
                         <StarRatings changeRating={rateTitle} rating={rating} starEmptyColor="#111111" starHoverColor="orange" starRatedColor="orange" starDimension="2.5em" starSpacing="0" svgIconViewBox="0 0 24 24"svgIconPath="M12 17.27l4.15 2.51c.76.46 1.69-.22 1.49-1.08l-1.1-4.72 3.67-3.18c.67-.58.31-1.68-.57-1.75l-4.83-.41-1.89-4.46c-.34-.81-1.5-.81-1.84 0L9.19 8.63l-4.83.41c-.88.07-1.24 1.17-.57 1.75l3.67 3.18-1.1 4.72c-.2.86.73 1.54 1.49 1.08l4.15-2.5z"/>
                         
-                        {/* TODO:
-                        - MAKE TITLE BIGGER AND ADD RELEASE YEAR AND RUN TIME UNDER
-                        ADD STAR WITH RATING NEXT TO IT WITH THE STARS  
-                        */}
                         
                         <div className="filmData-info">
                             <p className="title-name">{filmData.original_title}</p>
