9c4eaca6d1e522a8f315fd9bfcba7e7646e32a6d
Alex King <calicoalix@gmail.com>
2022-07-07 21:15:03 +1000

    fixed doubled up classnames

--- src/pages/Title.tsx
+++ src/pages/Title.tsx
@@ -132,7 +132,7 @@ function Title() {
                         
 
                         <div className="rating-stars">
-                            <div className="user-rate">
+                            <div className="user-stars">
                                 {/* User Score: */}
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" className="user-star" id="iconContext-star" viewBox="0 0 24 24" fill="currentColor" role="presentation"><path d="M12 17.27l4.15 2.51c.76.46 1.69-.22 1.49-1.08l-1.1-4.72 3.67-3.18c.67-.58.31-1.68-.57-1.75l-4.83-.41-1.89-4.46c-.34-.81-1.5-.81-1.84 0L9.19 8.63l-4.83.41c-.88.07-1.24 1.17-.57 1.75l3.67 3.18-1.1 4.72c-.2.86.73 1.54 1.49 1.08l4.15-2.5z"></path></svg>
                                 {filmData.vote_average / 2}/5 · {filmData.vote_count}
